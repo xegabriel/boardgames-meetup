ddd24b5c012a749e310e82d807dd374877c38090
mihail.stanescu <mihail.stanescu@metrosystems.net>
2020-01-15 21:38:28 +0200

    refactored code

--- bg-client/src/components/Events.js
+++ bg-client/src/components/Events.js
@@ -3,8 +3,7 @@ import '../styles/Events.css';
 import axios from "axios";
 import {API_HOST, TOKEN_COOKIE} from "../utils/constants";
 import Cookies from "universal-cookie";
-import HomeIcon from '@material-ui/icons/Home';
-import AccessTimeIcon from '@material-ui/icons/AccessTime';
+import EventCard from "./EventCard";
 
 class Events extends React.Component {
     constructor(props) {
@@ -30,19 +29,7 @@ class Events extends React.Component {
 
     render() {
         const eventsList = this.state.events.map((event) =>
-
-            <div className="card event-card" key={event.id.counter}>
-                <div className="card-body">
-                    <h5 className="card-title">{event.title}</h5>
-                    <h6 className="card-subtitle mb-2 text-muted">{event.location}</h6>
-                    <p className="card-text">{event.description}</p>
-                    <p><HomeIcon/> {event.fullAddress}</p>
-                    <p>
-                        <AccessTimeIcon/> {new Date(event.startDate).toLocaleString() + ' - ' + new Date(event.endDate).toLocaleString()}
-                    </p>
-                    <p className="card-link access-event">Read More</p>
-                </div>
-            </div>
+            <EventCard key={event.id.counter} event={event} />
         );
         return (<div className="events-body">{eventsList}</div>);
     }
