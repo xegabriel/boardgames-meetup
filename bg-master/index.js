baad466c720ab641917bba533eba0da953cc9b08
Viviana Munteanu <mmunteanu@lenovo.com>
2020-02-04 15:36:58 +0200

    Added index.js and GET /topUsers api

---
+++ bg-master/index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
+let db;
+
+MongoClient.connect('mongodb://vivi:1234@boardgamescluster-shard-00-00-2fdfc.gcp.mongodb.net:27017,boardgamescluster-shard-00-01-2fdfc.gcp.mongodb.net:27017,boardgamescluster-shard-00-02-2fdfc.gcp.mongodb.net:27017/test?ssl=true&replicaSet=BoardgamesCluster-shard-0&authSource=admin&retryWrites=true&w=majority', (err, database) => {
+  if (err) return console.log(err)
+  db = database.db('test')
+  app.listen(process.env.PORT || 3000, () => {
+    console.log('listening on 3000')
+  })
+})
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+app.use(express.static('public'))
+
+app.get('/topUsers', (req, res) => {
+  let noToReturn = 3;
+  if (req.query && req.query['no']) {
+    noToReturn = parseInt(req.query['no']);
+  }
+  console.log(`Returning top: ${noToReturn} users.`)
+  db.collection('users').find({}, { projection: { '_id': 0, 'email': 1, 'firstName': 1, 'lastName': 1, 'numberOfAttendedGames': 1 } }).limit(noToReturn).sort({ 'numberOfAttendedGames': -1 }).toArray((err, users) => {
+    if (err) {
+      console.error(`There was an error while trying to retrieve users. ${err}`);
+      res.status(500).send("ERROR")
+    } else {
+      console.log(`Found ${users.length} users.`)
+      res.status(200).send(users);
+    }
+  });
+});
